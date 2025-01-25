c187c14fa703eef4ec7e9a819969096787733d78
eliozeb <zebene.elias@hotmail.com>
2025-01-25 06:55:05 +0000

    chore: .gitignore update"

--- commands.bat
+++ commands.bat
@@ -1,19 +1,38 @@
-# Generate Heroku auth token
-heroku auth:token
+@echo off
+echo === Setting up Heroku PostgreSQL ===
 
-# Set Git remote with auth token
-git remote remove heroku
-git remote add heroku https://git.heroku.com/fastapi-elias.git
+REM Set app name
+SET APP_NAME=fastapi-elias
+SET DATABASE_HOSTNAME=localhost
+SET DATABASE_PORT=5432
+SET DATABASE_PASSWORD=saselias123
+SET DATABASE_NAME=fastapi
+SET DATABASE_USERNAME=postgres
+SET SECRET_KEY=09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c
+SET ALGORITHM=HS256
+SET ACCESS_TOKEN_EXPIRE_MINUTES=30
 
-# Store Heroku credentials
-git config credential.helper store
-heroku git:credentials
+REM Create PostgreSQL addon (mini tier for eco dynos)
+heroku addons:create heroku-postgresql:mini --app %APP_NAME%
 
-# Create required Heroku files
-echo web: uvicorn app.main:app --host=0.0.0.0 --port=$PORT > Procfile
-echo python-3.9.18 > runtime.txt
+REM Get database credentials
+heroku config:get DATABASE_URL --app %APP_NAME%
 
-# Commit and push
-git add .
-git commit -m "chore: add Heroku configuration files"
-git push heroku main
+REM Show database info
+heroku pg:info --app %APP_NAME%
+
+REM Set additional config vars
+heroku config:set DATABASE_USERNAME=%DATABASE_USERNAME% --app %APP_NAME%
+heroku config:set DATABASE_PASSWORD=%DATABASE_PASSWORD% --app %APP_NAME%
+heroku config:set DATABASE_NAME=%DATABASE_NAME% --app %APP_NAME%
+heroku config:set DATABASE_PORT=%DATABASE_PORT% --app %APP_NAME%
+heroku config:set DATABASE_URL=%DB_URL% --app %APP_NAME%
+heroku config:set SECRET_KEY=%SECRET_KEY% --app %APP_NAME%
+heroku config:set ALGORITHM=%ALGORITHM% --app %APP_NAME%
+heroku config:set ACCESS_TOKEN_EXPIRE_MINUTES=%ACCESS_TOKEN_EXPIRE_MINUTES% --app %APP_NAME%
+
+REM Show configuration
+heroku config --app %APP_NAME%
+
+echo Database setup complete!
+echo on
